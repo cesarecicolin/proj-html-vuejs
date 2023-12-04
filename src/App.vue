96b03bf216d2c8e4598182430af625af8618dc7e
cesarecicolin <cece.caster@gmail.com>
2023-12-04 12:14:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+  <h1>ciao bro</h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+</style>
